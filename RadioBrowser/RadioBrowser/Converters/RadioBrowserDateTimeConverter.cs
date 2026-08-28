815e89a052863b78488dff22fb6a8de55729c016
Michał Jaśkiewicz <michaljaskiewicz.developer@protonmail.com>
2026-08-28 17:37:48 +0200

    add

---
+++ RadioBrowser/RadioBrowser/Converters/RadioBrowserDateTimeConverter.cs
@@ -0,0 +1,37 @@
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using RadioBrowser.Exceptions;
+
+namespace RadioBrowser.Converters;
+
+public sealed class RadioBrowserDateTimeConverter : JsonConverter<DateTime>
+{
+    private const string ApiDateFormat = "yyyy-MM-dd HH:mm:ss";
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = GetDateValue(ref reader, nameof(DateTime));
+
+        if (DateTime.TryParseExact(value, ApiDateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out var dateTime) ||
+            DateTime.TryParse(value, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out dateTime))
+        {
+            return dateTime;
+        }
+
+        throw new RadioBrowserException($"Radio Browser returned an invalid DateTime value: '{value}'.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString("O", CultureInfo.InvariantCulture));
+    }
+
+    private static string GetDateValue(ref Utf8JsonReader reader, string typeName)
+    {
+        if (reader.TokenType != JsonTokenType.String)
+            throw new RadioBrowserException($"Radio Browser returned an invalid {typeName} token. Expected a string.");
+        return reader.GetString() ?? throw new RadioBrowserException($"Radio Browser returned a null {typeName} value.");
+    }
+}
--- RadioBrowser/RadioBrowser/Infrastructure/BaseClient.cs
+++ RadioBrowser/RadioBrowser/Infrastructure/BaseClient.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 
+using RadioBrowser.Converters;
 using RadioBrowser.Exceptions;
 
 using RestSharp;
@@ -8,39 +9,29 @@ namespace RadioBrowser.Infrastructure;
 
 public class BaseClient(IRestClient client)
 {
-    private static readonly JsonSerializerOptions DeserializeSettings = new();
+    private static readonly JsonSerializerOptions DeserializeSettings = new() { Converters = { new RadioBrowserDateTimeConverter(), new RadioBrowserDateTimeOffsetConverter() } };
 
     protected async Task<T> RequestAsync<T>(RestRequest request) where T : class
     {
         var res = await client.ExecuteAsync(request).ConfigureAwait(false);
 
         if (!res.IsSuccessful)
-        {
-            throw new RadioBrowserException(
-                $"[{request.Resource} => Status: {res.StatusCode}] Unsuccessful request!\nContent:\n{res.Content}",
-                res.StatusCode);
-        }
+            throw new RadioBrowserException($"[{request.Resource} => Status: {res.StatusCode}] Unsuccessful request!\nContent:\n{res.Content}", res.StatusCode);
 
         if (string.IsNullOrWhiteSpace(res.Content))
-        {
-            throw new RadioBrowserException(
-                $"[{request.Resource} => Status: {res.StatusCode}] Received empty response!", res.StatusCode);
-        }
+            throw new RadioBrowserException($"[{request.Resource} => Status: {res.StatusCode}] Received empty response!", res.StatusCode);
 
         try
         {
             var model = JsonSerializer.Deserialize<T>(res.Content, options: DeserializeSettings) ??
-                        throw new RadioBrowserException(
-                            $"Failed to deserialize [{request.Resource}] response! Content parsed to null.",
-                            res.StatusCode);
+                        throw new RadioBrowserException($"Failed to deserialize [{request.Resource}] response! Content parsed to null.", res.StatusCode);
 
 
             return model;
         }
         catch (JsonException ex)
         {
-            throw new RadioBrowserException(
-                $"Failed to deserialize [{request.Resource}] response due to invalid JSON format!", ex);
+            throw new RadioBrowserException($"Failed to deserialize [{request.Resource}] response due to invalid JSON format!", ex);
         }
     }
 }
